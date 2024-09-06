f2a836c51c0b4fe580013c524deecaf8d1284d4a
Piero Maddaleni <piero@piemadd.com>
2024-09-06 11:23:30 -0500

    updated!

--- scripts/fetchAdditionalShapes.js
+++ scripts/fetchAdditionalShapes.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const passio = require('../additionalShapes/passio.json');
 const amtrak = require('../additionalShapes/amtrak.json');
 const snowPiercer = require('../additionalShapes/snowPiercer.json');
+const bigboyPart1 = require('../additionalShapes/bigboypart1.json');
+const bigboyPart2 = require('../additionalShapes/bigboypart2.json');
 
 passio.forEach((agency) => {
   fetch(`https://passio.piemadd.com/data/${agency}`)
@@ -16,3 +18,5 @@ passio.forEach((agency) => {
 
 fs.writeFileSync('./shapes/nationalRoute.json', JSON.stringify(amtrak), { encoding: 'utf8' });
 fs.writeFileSync('./shapes/snowPiercer.json', JSON.stringify(snowPiercer), { encoding: 'utf8' });
+fs.writeFileSync('./shapes/bigboypart1.json', JSON.stringify(bigboyPart1), { encoding: 'utf8' });
+fs.writeFileSync('./shapes/bigboypart2.json', JSON.stringify(bigboyPart2), { encoding: 'utf8' });
