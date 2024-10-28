4ca4a3f6c415e24300401a5a1803a2d1803bd385
piemadd <piero@piemadd.com>
2024-10-27 19:20:14 -0500

    ok all done

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # gobbler
 
 Gobbler is intended to be a partial rewrite of `gtfs-schedule-data` with a focus on modular systems, allowing for fast and efficient GTFS ingestion while moving the most intensive parts of `gtfs-schedule-data` to a separate service.
+
+amtrak.json: https://geodata.bts.gov/datasets/8f534a0c7468483f9837ef93e2e5ebb1_0/explore?location=37.679052%2C-95.434899%2C4.90
--- scripts/fetchAdditionalShapes.js
+++ scripts/fetchAdditionalShapes.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const passio = require('../additionalShapes/passio.json');
 const amtrak = require('../additionalShapes/amtrak.json');
 const snowPiercer = require('../additionalShapes/snowPiercer.json');
-const bigboyPart1 = require('../additionalShapes/bigboypart1.json');
-const bigboyPart2 = require('../additionalShapes/bigboypart2.json');
 
 passio.forEach((agency) => {
   fetch(`https://passio.piemadd.com/data/${agency}`)
@@ -18,5 +16,3 @@ passio.forEach((agency) => {
 
 fs.writeFileSync('./shapes/nationalRoute.json', JSON.stringify(amtrak), { encoding: 'utf8' });
 fs.writeFileSync('./shapes/snowPiercer.json', JSON.stringify(snowPiercer), { encoding: 'utf8' });
-fs.writeFileSync('./shapes/bigboypart1.json', JSON.stringify(bigboyPart1), { encoding: 'utf8' });
-fs.writeFileSync('./shapes/bigboypart2.json', JSON.stringify(bigboyPart2), { encoding: 'utf8' });
