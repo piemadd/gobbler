9651e38b72f74bcc2134504f727e79685040b25c
piemadd <piero@piemadd.com>
2025-02-26 08:28:53 -0600

    new versions

--- ingest copy.sh
+++
@@ -1,3 +0,0 @@
-#tippecanoe -z12 --projection=EPSG:4326 --force -o transit.pmtiles -l zcta all.geojson
-#tippecanoe -z12 --projection=EPSG:4326 --force -o amtrakerPre.pmtiles -l zcta allAmtraker.geojson
-./pmtiles extract amtrakerPre.pmtiles amtraker.pmtiles --minzoom 12
