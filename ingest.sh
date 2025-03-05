432aa4c4fecd3d4d227ef7c6ead45d13aafa401f
piemadd <piero@piemadd.com>
2025-03-05 05:45:07 -0600

    made amtraker.pmtiles actually work

--- ingest.sh
+++ ingest.sh
@@ -1,2 +1,2 @@
 #tippecanoe -z12 --projection=EPSG:4326 --force -o transit.pmtiles -l zcta all.geojson
-tippecanoe -z0 -Z0 -d12 -D12 -m12 --projection=EPSG:4326 --force -o amtraker.pmtiles -l zcta allAmtraker.geojson
+tippecanoe -z12 --projection=EPSG:4326 --force -o amtraker.pmtiles -l zcta allAmtraker.geojson
