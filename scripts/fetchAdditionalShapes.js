3dfe0d80104bc9db7e2f3bf3518ff492054310e5
piemadd <piero@piemadd.com>
2025-11-21 01:13:44 -0600

    added cpkc holiday shape

--- scripts/fetchAdditionalShapes.js
+++ scripts/fetchAdditionalShapes.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
-const passio = require('../additionalShapes/passio.json');
+//const passio = require('../additionalShapes/passio.json');
 const amtrak = require('../additionalShapes/amtrak.json');
 const amtrak_capitol_corridor = require('../additionalShapes/amtrak_capitol_corridor.json');
 const amtrak_cascades = require('../additionalShapes/amtrak_cascades.json');
 const amtrak_mardi_gras = require('../additionalShapes/amtrak_mardi_gras.json');
 const snowPiercer = require('../additionalShapes/snowPiercer.json');
 const mff = require('../additionalShapes/mff.json');
+const cpkcHoliday = require('../additionalShapes/cpkcHoliday.json');
 
+/*
 (async () => {
   for (let i = 0; i < passio.length; i++) {
     const agency = passio[i];
@@ -16,6 +18,7 @@ const mff = require('../additionalShapes/mff.json');
     console.log(`Fetched ${agencyKey}`);
   }
 })();
+*/
 
 fs.writeFileSync('./shapes/nationalRoute.json', JSON.stringify(amtrak), { encoding: 'utf8' });
 fs.writeFileSync('./shapes/amtrak_capitol_corridor.json', JSON.stringify(amtrak_capitol_corridor), { encoding: 'utf8' });
@@ -23,3 +26,4 @@ fs.writeFileSync('./shapes/amtrak_cascades.json', JSON.stringify(amtrak_cascades
 fs.writeFileSync('./shapes/amtrak_mardi_gras.json', JSON.stringify(amtrak_mardi_gras), { encoding: 'utf8' });
 fs.writeFileSync('./shapes/snowPiercer.json', JSON.stringify(snowPiercer), { encoding: 'utf8' });
 fs.writeFileSync('./shapes/mff.json', JSON.stringify(mff), { encoding: 'utf8' });
+fs.writeFileSync('./shapes/cpkcHoliday.json', JSON.stringify(cpkcHoliday), { encoding: 'utf8' });
